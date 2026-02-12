3604809d17fc27bb9bce2a9595f00f408449c7ca
SouOWendel <souowendel@gmail.com>
2026-02-12 00:34:57 -0300

    feat(layout): update navigation structure and main container styles

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './views/home/home.component';
-import { AboutComponent } from './views/about/about.component';
+import { HomeComponent } from './home/home';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
 	{ path: '', component: HomeComponent },
